f0020cd490cfc7f33f2fab62e61ad5ba9b7af36b
tamarmov <87264825+tamarmov@users.noreply.github.com>
2022-02-17 00:11:44 +0200

    Add files via upload

---
+++ CustomerBl.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BLL.Converters;
+using DAL;
+using DTO;
+using conv = BLL.Converters.CustomerConverters;
+
+namespace BLL
+{
+    public class CustomerBl
+    {
+        
+        public List<CustomerDTO> GetAll()
+        {
+            var list = new DAL.CustomerDal().GetAll();
+            List<CustomerDTO> DTOlist = new List<CustomerDTO>();
+
+            //foreach (var item in list)
+            //{
+            //    //yield return Converters.CustomerConverters.GetCustomerEntityFromDTO(item);
+
+            //    list.Add(new conv().GetCustomerEntityFromDTO(item));
+            //}
+            return list.Select(x => new CustomerDTO()
+            {
+                CustomerId= x.CustomerId,
+                CustomerAdress = x.CustomerAdress,
+                CustomerEmail = x.CustomerEmail,
+                CustomerName = x.CustomerName,
+                CustomerStatus = x.CustomerStatus,
+                CustomerTel = x.CustomerTel
+            }).ToList();
+            //return (IEnumerable <CustomerDTO > )list;
+        }
+
+        //public Customer GetCustomer(int id)
+        //{
+        //    Customer v = new DAL.CustomerDal().GetCustomer(id);
+        //    return
+        //        (new Converters.CustomerConverters()).GetCustomerEntityFromDTO(
+        //     (new CustomerDal().GetCustomer(id)));
+        //}
+
+        public int AddCustomer(CustomerDTO model)
+        {
+            //Converters.AssistanceConverters.ConvertEntity<Assistance, AssistanceDTO>( );
+            return new DAL.CustomerDal().AddCustomer(
+                new Customer()
+                {
+                    CustomerAdress = model.CustomerAdress,
+                    CustomerEmail = model.CustomerEmail,
+                    CustomerName = model.CustomerName,
+                    CustomerStatus = model.CustomerStatus,
+                    CustomerTel = model.CustomerTel,
+                });
+        }
+
+        //public CustomerDTO GetLogin(LoginData loginData)
+        //{
+        //    return (new Converters.CustomerConverters()).GetCustomerFromEntity(new DAL.CustomerDal()
+        //        .GetCustomerByIdAndEmail(loginData.mail, loginData.password));
+        //}‏
+    }
+}
---
+++ ProductBl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DTO;
+
+namespace BLL
+{
+    public class ProductBl
+    {
+        public List<ProductDTO> GetAll()
+        {
+            var list = new DAL.ProductDal().GetAll();
+            List<ProductDTO> DTOlist = new List<ProductDTO>();
+
+            //foreach (var item in list)
+            //{
+            //    //yield return Converters.CustomerConverters.GetCustomerEntityFromDTO(item);
+
+            //    list.Add(new conv().GetCustomerEntityFromDTO(item));
+            //}
+            DTOlist= list.Select(x => new ProductDTO()
+            {
+                ProductId = x.ProductId,
+                ProductBusinessPrice = x.ProductBusinessPrice,
+                ProductDescription = x.ProductDescription,
+                ProductPrice = x.ProductPrice,
+                ProductStatus = x.ProductStatus
+            }).ToList();
+            return DTOlist;
+            //return (IEnumerable <CustomerDTO > )list;
+        }
+
+        public ProductDTO GetProduct(int id)
+        {
+            //Converters.AssistanceConverters.ConvertEntity<Assistance, AssistanceDTO>( );
+
+            return (new Converters.PoductConverters()).GetProductEntityFromDTO(
+             (new DAL.ProductDal().GetProductById(id)));
+        }
+
+        public int AddProduct(ProductDTO model)
+        {
+            //Converters.AssistanceConverters.ConvertEntity<Assistance, AssistanceDTO>( );
+            return new ProductDal().AddProduct(
+                new Product()
+                {
+                    ProductId = model.ProductId,
+                    ProductBusinessPrice = model.ProductBusinessPrice,
+                    ProductDescription = model.ProductDescription,
+                    ProductPrice = model.ProductPrice,
+                    ProductStatus = model.ProductStatus
+                });
+        }
+    }
+}
